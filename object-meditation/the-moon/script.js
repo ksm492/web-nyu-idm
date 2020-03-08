73f475cd8f495d3e54763bfa6c1c35c6b1cd46cb
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-03-08 16:50:41 -0400

    added jquery to moon

--- object-meditation/the-moon/script.js
+++ object-meditation/the-moon/script.js
@@ -1,1 +1,18 @@
-console.log("I'm here, I'm here!!")
+$(document).ready(function() {
+
+  $(".night").click(function() {
+    $("body").css({
+      "background-color": "#1e1d2a"});
+    $(".night").css({
+      "color": "white"});
+  });
+
+  $("#heart").click(function() {
+    var windowHeight = 1920;
+
+    $("#heart").fadeIn()
+    .css({position:'relative'})
+    .animate({top:windowHeight}, 1000, function()
+    { $('#heart').removeAttr('style'); });
+    });
+});
