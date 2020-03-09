0095d4150be3bc24a4899ecfaed7aaf0ed6d98d5
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-03-09 14:00:36 -0400

    added mobile version to sun

--- object-meditation/the-sun/script.js
+++ object-meditation/the-sun/script.js
@@ -1,1 +1,34 @@
-console.log("I'm here, I'm here!!")
+$(document).ready(function() {
+
+  // jQuery for WEBSITE
+
+  $("div.day").click(function() {
+    $("body").css({
+      "background-color": "#fa7976"});
+    $(".day").css({
+      "color": "white"});
+  });
+
+  $("#heart").click(function() {
+    var windowHeight = $(window).height();
+
+    $("#heart").fadeIn()
+    .css({position:'relative'})
+    .animate({top:-windowHeight}, 1000, function()
+    { $('#heart').removeAttr('style'); });
+    });
+
+    // jQuery for MOBILE
+
+    $("#dream-btn").click(function(){
+   		$(".dream-gif").toggle();
+	});
+
+    $("#miss-btn").click(function(){
+      $(".miss-gif").toggle();
+  });
+
+    $("#think-btn").click(function(){
+      $(".think-gif").toggle();
+  });
+});
