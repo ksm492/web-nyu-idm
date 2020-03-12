8a1025088950e3d89f66518f7525e5c6dee2d837
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-03-12 15:55:27 -0400

    1. I found the length of the array

--- javascript-playground/challenge.js
+++ javascript-playground/challenge.js
@@ -5,11 +5,15 @@ let myAlphabet = ['A', 'B', 'C', 'D','E','F','G'];
 // 1. What is the length of the array?
 // push your edits to github!!
 
+console.log(myAlphabet.length);
+
 // 2. Write a function called myAlphabetLength which console.logs the length of the array
 // push your edits to github!!
 
 // 3. Within the function also use an if-conditional statement that checks if the number of items within the array are less than 4
 // push your edits to github!!
+
+// *STOP
 
 // 4. Declare and initialize an array called 'colors' with 5 different string values representing the names of 5 colors of your choosing
 // push your edits to github!!
--- javascript-playground/demo.js
+++ javascript-playground/demo.js
@@ -3,3 +3,31 @@
 // watch later: https://www.youtube.com/watch?v=XgSjoHgy3Rk
 
 let namesArray = ['Kevin','McCoy','Emily','Frederic','Alisha','Ashli','Simon','Ty','Fiona','Prismo','Sammy','Caroline','Francesca','Karl','Esther','Irene','Martin','Yanxin','Katrina','Kiana'];
+
+// console.log(namesArray[0]);
+
+function getRandom(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+
+//ARRAYS START FROM ZERO, NOT ONE
+
+// console.log(Math.random());
+// console.log(getRandom(1));
+// console.log(getRandom(3));
+
+let n = getRandom(namesArray.length);
+let randomName = namesArray[n];
+
+
+console.log(namesArray.length);
+// console.log(n);
+console.log(randomName);
+
+let arrayAsString = namesArray.join(", ");
+console.log(arrayAsString);
+console.log(namesArray);
+
+$("#name").html(namesArray[n]);
+
+$("#names").html(arrayAsString);
