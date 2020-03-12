1318d35c1134dc662d0eb9057b898211564190cb
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-03-12 16:12:18 -0400

    2. I wrote a function that console.logs the length of the array

--- javascript-playground/demo.js
+++ javascript-playground/demo.js
@@ -2,32 +2,32 @@
 // Understanding the difference between let & var: It can be said that a variable declared with var is defined throughout the program (function scoped) as compared to let (which is block scoped). A function can contain many blocks.
 // watch later: https://www.youtube.com/watch?v=XgSjoHgy3Rk
 
-let namesArray = ['Kevin','McCoy','Emily','Frederic','Alisha','Ashli','Simon','Ty','Fiona','Prismo','Sammy','Caroline','Francesca','Karl','Esther','Irene','Martin','Yanxin','Katrina','Kiana'];
-
-// console.log(namesArray[0]);
-
+// let namesArray = ['Kevin','McCoy','Emily','Frederic','Alisha','Ashli','Simon','Ty','Fiona','Prismo','Sammy','Caroline','Francesca','Karl','Esther','Irene','Martin','Yanxin','Katrina','Kiana'];
+//
+// // console.log(namesArray[0]);
+//
 function getRandom(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
-
-//ARRAYS START FROM ZERO, NOT ONE
-
-// console.log(Math.random());
-// console.log(getRandom(1));
-// console.log(getRandom(3));
-
-let n = getRandom(namesArray.length);
-let randomName = namesArray[n];
-
-
-console.log(namesArray.length);
-// console.log(n);
-console.log(randomName);
-
-let arrayAsString = namesArray.join(", ");
-console.log(arrayAsString);
-console.log(namesArray);
-
-$("#name").html(namesArray[n]);
-
-$("#names").html(arrayAsString);
+//
+// //ARRAYS START FROM ZERO, NOT ONE
+//
+// // console.log(Math.random());
+// // console.log(getRandom(1));
+// // console.log(getRandom(3));
+//
+// let n = getRandom(namesArray.length);
+// let randomName = namesArray[n];
+//
+//
+// console.log(namesArray.length);
+// // console.log(n);
+// console.log(randomName);
+//
+// let arrayAsString = namesArray.join(", ");
+// console.log(arrayAsString);
+// console.log(namesArray);
+//
+// $("#name").html(namesArray[n]);
+//
+// $("#names").html(arrayAsString);
