462442bf97a7c2a3717b938c987c376028347320
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-03-26 14:31:58 -0500

    5. I added a for loop to console.log each item in the array

--- javascript-playground/challenge.js
+++ javascript-playground/challenge.js
@@ -39,8 +39,14 @@ let colors = ["red", "orange", "yellow", "green", "blue"];
 // 5.  Console.log each item in the array
 // push your edits to github!!
 
+for (i=0; i < colors.length; i++) {
+  console.log(colors[i]);
+}
+
 // 6. Declare an empty string variable called 'text'. Each time you console.log a color from the above challenge number 2, add your color to this variable.
 // push your edits to github!!
+
+
 
 //7. Create a div in the index.html with a unique id called whatever you want. Display the contents of your text variable to this div. Now you should have a list of 5 colors visible on your website.
 // push your edits to github!!
