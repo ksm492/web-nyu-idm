ebbfcf2813bde21d66e1862c75fbe3ee86514649
kmenguita <56565345+kmenguita@users.noreply.github.com>
2020-03-26 14:28:17 -0500

    4. I wrote an array that has a 5 strings names after 5 colors

--- javascript-playground/demo.js
+++ javascript-playground/demo.js
@@ -2,32 +2,41 @@
 // Understanding the difference between let & var: It can be said that a variable declared with var is defined throughout the program (function scoped) as compared to let (which is block scoped). A function can contain many blocks.
 // watch later: https://www.youtube.com/watch?v=XgSjoHgy3Rk
 
-// let namesArray = ['Kevin','McCoy','Emily','Frederic','Alisha','Ashli','Simon','Ty','Fiona','Prismo','Sammy','Caroline','Francesca','Karl','Esther','Irene','Martin','Yanxin','Katrina','Kiana'];
-//
-// // console.log(namesArray[0]);
-//
-// function getRandom(max) {
-//   return Math.floor(Math.random() * Math.floor(max));
-// }
-//
-// //ARRAYS START FROM ZERO, NOT ONE
-//
-// // console.log(Math.random());
-// // console.log(getRandom(1));
-// // console.log(getRandom(3));
-//
-// let n = getRandom(namesArray.length);
-// let randomName = namesArray[n];
-//
-//
+let namesArray = ['Kevin','McCoy','Emily','Frederic','Alisha','Ashli','Simon','Ty','Fiona','Prismo','Sammy','Caroline','Francesca','Karl','Esther','Irene','Martin','Yanxin','Katrina','Kiana'];
+// ARRAYS START FROM ZERO NOT ONE
+// console.log(namesArray[0]);
+function getRandom(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+// console.log(Math.random());
+// console.log(getRandom(1));
+// console.log(getRandom(3));
+// Expected output is 0, 1, or 2 (not 3)
+let n = getRandom(namesArray.length)
+let randomName = namesArray[n];
 // console.log(namesArray.length);
-// // console.log(n);
+// console.log(n);
 // console.log(randomName);
-//
-// let arrayAsString = namesArray.join(", ");
-// console.log(arrayAsString);
+let arrayAsString = namesArray.join("🥕");
 // console.log(namesArray);
-//
-// $("#name").html(namesArray[n]);
-//
-// $("#names").html(arrayAsString);
+// console.log(arrayAsString);
+
+let sortedNames = namesArray.sort();
+console.log(sortedNames);
+
+let text = "";
+
+// text += namesArray[0] + "<br>";
+// text += namesArray[1] + "<br>";
+// text += namesArray[2] + "<br>";
+// text += namesArray[3] + "<br>";
+// text += namesArray[4] + "<br>";
+
+for (i=0; i<namesArray.length; i++) {
+  text += namesArray[i] + " 🍅 " + "<br>";
+}
+
+$("#listNames").html(text);
+
+$('#name').html(namesArray[n]);
+$('#names').html(arrayAsString);
